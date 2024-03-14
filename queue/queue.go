3c9511f243fbe03129508f5531345e0b03085547
vblinden <hello@vblinden.dev>
2024-03-14 20:51:51 +0100

    Add queue

---
+++ queue/queue.go
@@ -0,0 +1,32 @@
+package queue
+
+import "sync"
+
+// TODO: Introduce Peek and Size methods...
+
+type Queue[T any] struct {
+	Items []T
+	lock  sync.Mutex
+}
+
+func (q *Queue[T]) Push(i T) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	q.Items = append(q.Items, i)
+}
+
+func (q *Queue[T]) Pop() T {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	if len(q.Items) == 0 {
+		var zero T
+		return zero
+	}
+
+	item := q.Items[0]
+	q.Items = q.Items[1:]
+
+	return item
+}
