6591df59483f7b0da06e6e04b101b6fc333483f7
vblinden <hello@vblinden.dev>
2024-03-22 16:44:00 +0100

    Add frequently used helper methods

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -15,3 +15,23 @@ func (s *Scheduler) Start(interval time.Duration, f func()) {
 		}
 	}
 }
+
+func (s *Scheduler) EveryMinute(f func()) {
+	s.Start(time.Minute, f)
+}
+
+func (s *Scheduler) EveryFiveMinutes(f func()) {
+	s.Start(5*time.Minute, f)
+}
+
+func (s *Scheduler) EveryFifteenMinutes(f func()) {
+	s.Start(15*time.Minute, f)
+}
+
+func (s *Scheduler) EveryThirtyMinutes(f func()) {
+	s.Start(30*time.Minute, f)
+}
+
+func (s *Scheduler) EveryHour(f func()) {
+	s.Start(time.Hour, f)
+}
